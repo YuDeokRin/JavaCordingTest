041c449844f2a10248e5de523523df28b3d179f5
YuDeokRin <ejrfls12@naver.com>
2022-01-01 17:26:01 +0900

    Initial commit

---
+++ src/string01/str01.java
@@ -0,0 +1,30 @@
+package string01;
+
+
+
+import java.util.Scanner;
+
+public class str01 {
+    public int solution(String str, char t){
+        int answer = 0 ;
+        str=str.toUpperCase(); // str이 모두 대문자가 된다.
+        t = Character.toUpperCase(t);
+
+        // toCharArray()는 문자배열로 만들어주는 메소드
+        for (char  x : str.toCharArray()) {
+            if(x ==t ) answer++;
+        }
+
+        return answer;
+
+    }
+    public static void main(String[] args){
+        str01 T = new str01();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+
+        System.out.print(T.solution(str, c));
+
+    }
+}
