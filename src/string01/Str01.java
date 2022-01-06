faeb1fa3727f82507c27814e383456ef79b6fdd9
YuDeokRin <ejrfls12@naver.com>
2022-01-06 15:02:47 +0900

    05.특정 문자 뒤집기(toCharArray())

--- src/string01/Str01.java
+++ src/string01/Str01.java
@@ -9,14 +9,11 @@ public class Str01 {
         int answer = 0 ;
         str=str.toUpperCase(); // str이 모두 대문자가 된다.
         t = Character.toUpperCase(t);
-
         // toCharArray()는 문자배열로 만들어주는 메소드
         for (char  x : str.toCharArray()) {
             if(x ==t ) answer++;
         }
-
         return answer;
-
     }
     public static void main(String[] args){
         Str01 T = new Str01();
