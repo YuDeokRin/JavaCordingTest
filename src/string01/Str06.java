ccef20047b6c25c088f565bd6b23cdc70def2e94
YuDeokRin <ejrfls12@naver.com>
2022-01-08 23:20:36 +0900

    06. 중복문자제거

---
+++ src/string01/Str06.java
@@ -0,0 +1,25 @@
+package string01;
+
+import sun.applet.Main;
+
+import java.util.Scanner;
+
+public class Str06 {
+     public String solution(String str){
+         String answer = "";
+         for (int i =0; i<str.length(); i++){
+             //System.out.println(str.charAt(i)+ " " + str.indexOf(str.charAt(i)));
+             if (str.indexOf(str.charAt(i))==i) answer+= str.charAt(i);
+         }
+         return answer;
+     }
+
+    public static void main(String[] args) {
+        Str06 T = new Str06();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
+
+
