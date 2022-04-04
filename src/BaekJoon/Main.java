ba8f86f7b23e3fc6c6fecb6a5bc94f4e6a11526a
YuDeokRin <ejrfls12@naver.com>
2022-04-05 01:18:32 +0900

    중복문자제거

--- src/BaekJoon/Main.java
+++ src/BaekJoon/Main.java
@@ -2,15 +2,26 @@ package BaekJoon;
 
 import java.util.Scanner;
 
+
 public class Main {
+    private String solution(String str) {
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            //System.out.println(str.charAt(i) +"---- " + i +  "---- " + str.indexOf(str.charAt(i)));
+            if (str.indexOf(str.charAt(i))==i){
+                answer = answer + str.charAt(i); // i를 누적해준다.
+            }
+        }
+
+
+        return answer;
+    }
+
     public static void main(String[] args) {
-
+        Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-
-        System.out.print(A - B);
-
+        String str = sc.next();
+        System.out.println(T.solution(str));
     }
 }
 
---
+++ src/ExString/StringEx06.java
@@ -0,0 +1,10 @@
+package ExString;
+
+/**
+ * 중복문자제거
+ *
+ *
+ */
+public class StringEx06 {
+
+}
