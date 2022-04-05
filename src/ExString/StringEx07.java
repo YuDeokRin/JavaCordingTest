40177aba09bdc308cd3d6b4cfd94ef462bae3daf
YuDeokRin <ejrfls12@naver.com>
2022-04-05 20:53:23 +0900

    회문문자열

---
+++ src/ExString/StringEx07.java
@@ -0,0 +1,26 @@
+package ExString;
+
+import java.util.Scanner;
+
+public class StringEx07 {
+    public String solution(String str) {
+        String answer = "YES";
+        str = str.toUpperCase();
+        int len  =  str.length();
+        for (int i = 0; i < len/2; i++) {
+            if (str.charAt(i) != str.charAt(len-i-1)){
+                return "NO";
+            }
+        }
+
+        return answer ;
+    }
+
+    public static void main(String[] args) {
+        StringEx07 T = new StringEx07();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(T.solution(str));
+
+    }
+}
--- src/string01/Str07.java
+++ src/string01/Str07.java
@@ -1,6 +1,16 @@
 package string01;
 
-import java.util.Locale;
+/**
+ * 회문 문자열
+ * 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 한다.
+ *
+ *  Tip. length/2
+ *      다른 풀이 방법 : Stringbuilder().reverse().toString();
+ *                    String.equals()
+ *                    String.equalsIgnoreCase()
+ */
+
+
 import java.util.Scanner;
 
 public class Str07 {
