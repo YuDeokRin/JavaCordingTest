a3cb603a902f6aca4be54eb725104d9730ef07fc
YuDeokRin <ejrfls12@naver.com>
2022-04-13 20:43:59 +0900

    소수(에라토테네스 체)

--- src/BaekJoon/Main.java
+++ src/BaekJoon/Main.java
@@ -1,32 +1,41 @@
 package BaekJoon;
 
 
+/**
+ * 연습장
+ * 1. 부터 까지의 자연수를 전부 나열한다.  n = 20;
+ *
+ * 2. 소수도, 합성수도 1 아닌 을 지운다.
+ *
+ * 3. 남아 있는 자연수 중 가장 작은 2 수인 는 소수다. 이제 2의 배수들을 모두 지운다.
+ *
+ * 4. 남아 있는 자연수 중 가장 작은 3 수인 은 소수다. 이제 3의 배수들을 모두 지운다.
+ *
+ * 5. 남아 있는 자연수 중 가장 작은 수는 소수다. 이 수의 배수들을 모두 지운다.
+ *
+ *
+ */
 
-import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
-        public int solution(int n, int[] arr) {
-            int answer = 1;
-            int max = arr[0];
-            for (int i = 1; i < n; i++) {
-                if (arr[i] > max){
-                    max = arr[i];
-                    answer++;
+    public int solution(int n){
+        int answer = 0;
+        int ch[] = new int[n+1];
+        for (int i = 2; i <= n; i++) {
+            if (ch[i] == 0){
+                answer++;
+                for (int j = i; j < n ; j = j + 1) {
                 }
             }
-            return answer;
         }
+        return answer ;
+    }
 
         public static void main(String[] args) {
             Main T = new Main();
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = sc.nextInt();
-            }
-                System.out.print(T.solution(n, arr));
-
+            System.out.println(T.solution(n));
         }
 }
 
