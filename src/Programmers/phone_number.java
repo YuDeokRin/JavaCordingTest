aae7bbcf238746621ab66a4064a31b036d804dcc
YuDeokRin <ejrfls12@naver.com>
2022-04-21 21:22:17 +0900

    핸드폰 번호 가리기

---
+++ src/Programmers/phone_number.java
@@ -0,0 +1,31 @@
+package Programmers;
+/**
+ * 핸드폰 번호 가리기
+ *
+ */
+
+import java.util.Scanner;
+
+public class phone_number {
+    public String solution(String str) {
+        String answer="";
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length()-4){
+                answer+= "*";
+            }else{
+                answer += str.charAt(i);
+            }
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        phone_number T = new phone_number();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(T.solution(str));
+
+
+    }
+}
