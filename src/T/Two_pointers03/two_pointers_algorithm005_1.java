ecc2a5a0faa0c7df02068d062f44ace670674139
YuDeokRin <ejrfls12@naver.com>
2022-04-22 21:06:56 +0900

    5.연속된 자연수의 합(수학)

---
+++ src/T/Two_pointers03/two_pointers_algorithm005_1.java
@@ -0,0 +1,32 @@
+package T.Two_pointers03;
+
+/**
+ * 5. 연속된 자연수의 합(수학)
+ */
+
+import java.util.Scanner;
+
+public class two_pointers_algorithm005_1 {
+    public int solution(int n) {
+        int answer = 0 ;
+        int cnt =1;
+        n--;
+        while (n>0){
+            cnt++;
+            n = n - cnt;
+            if(n%cnt==0){
+                System.out.println(n + " " + cnt);
+                answer++;
+            }
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        two_pointers_algorithm005_1 T = new two_pointers_algorithm005_1();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(T.solution(n));
+    }
+}
