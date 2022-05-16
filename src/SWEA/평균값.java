93ae93b7148f96fd57f8add7307e54808b8c6ed8
YuDeokRin <ejrfls12@naver.com>
2022-05-16 21:44:58 +0900

    2071. 평균값 구하기

---
+++ src/SWEA/평균값.java
@@ -0,0 +1,25 @@
+package SWEA;
+import java.util.Scanner;
+
+
+class 평균값 {
+    public void average(int[] arr){
+
+    }
+    public static void main(String args[]) throws Exception{
+        평균값 T = new 평균값();
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        int[] arr = new int[10];
+        long sum = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                arr[j] = sc.nextInt();
+                sum+=arr[j];
+            }
+            System.out.println();
+            long avg = sum/10;
+            System.out.println("#" +i +" "+ avg);
+        }
+    }
+}
