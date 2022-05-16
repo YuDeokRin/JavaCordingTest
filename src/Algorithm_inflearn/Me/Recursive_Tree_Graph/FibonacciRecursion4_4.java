9450045e51da86cbe1d5235139ed503553e174f1
YuDeokRin <ejrfls12@naver.com>
2022-05-16 21:42:10 +0900

    4.피보나치수열(재귀풀이)

---
+++ src/Algorithm_inflearn/Me/Recursive_Tree_Graph/FibonacciRecursion4_4.java
@@ -0,0 +1,31 @@
+package Algorithm_inflearn.Me.Recursive_Tree_Graph;
+/**
+ * 4. 피보나치 수열 (재귀함수 사용)
+ *  재귀함수 중 효율이 제일 좋음.
+ */
+
+import java.util.Scanner;
+
+public class FibonacciRecursion4_4 {
+    static int fibo[];
+
+    public int DFS(int n) {
+        if (fibo[n] >0) return fibo[n];
+        if (n == 1) return fibo[n] = 1;
+        else if (n == 2) return fibo[n] = 1;
+        else return fibo[n] = DFS(n - 2) + DFS(n - 1);
+
+
+    }
+
+    public static void main(String[] args) {
+        FibonacciRecursion4_4 T = new FibonacciRecursion4_4();
+        int  n = 45;
+        fibo = new int[n + 1];
+        T.DFS(n);
+        for (int i = 2; i <= n; i++) {
+            System.out.println(fibo[i] + " ");
+        }
+        System.out.print(T.DFS(n));
+    }
+}
