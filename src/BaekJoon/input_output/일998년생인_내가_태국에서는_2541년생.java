7a84314d55639f1a585dad15f1a8451f24925028
YuDeokRin <ejrfls12@naver.com>
2022-06-14 17:27:43 +0900

    18108. 1998년생인 내가 태국에서는 2541년생

---
+++ src/BaekJoon/input_output/일998년생인_내가_태국에서는_2541년생.java
@@ -0,0 +1,17 @@
+package BaekJoon.input_output;
+
+/*
+ 18108. 1998년생인 내가 태국에서는 2541년생
+
+ 간단한 입출력 문제
+ */
+import java.util.Scanner;
+
+public class 일998년생인_내가_태국에서는_2541년생 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(n - 543);
+
+    }
+}
