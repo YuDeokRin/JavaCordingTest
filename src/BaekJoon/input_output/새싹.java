3d9a33c269043bd793d6eaac4344dcbf7e11f64a
YuDeokRin <ejrfls12@naver.com>
2022-06-14 17:28:08 +0900

    25083. 새싹

---
+++ src/BaekJoon/input_output/새싹.java
@@ -0,0 +1,27 @@
+package BaekJoon.input_output;
+/*
+25083. 새싹
+
+알고리즘 분류 : 구현
+
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+
+위에 새싹 출력
+
+ */
+public class 새싹 {
+    public static void main(String[] args) {
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r' ");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+
+}
