3bb789553d8ec17ebe00936ab4552cbfaebc7309
YuDeokRin <ejrfls12@naver.com>
2022-12-03 19:38:20 +0900

    Submission - 위장 (solution2 -stream 해결법 및 리팩토링)

--- src/ProgrammersSchool/week3/위장.java
+++ src/ProgrammersSchool/week3/위장.java
@@ -31,14 +31,24 @@ public class 위장 {
             2. Stream으로 구현
          */
         public int solution2(String[][] clothes){
-            int answer = Arrays.stream(clothes)
+            int answer = Arrays.stream(clothes) // 모든 옷의 종류
+                    .map(c -> c[1]) // 모든 옷에 1번 인덱스에 있는 종류들만 꺼낸다.
+                    .distinct() // 중복없이
+                    .map(type -> (int)Arrays.stream(clothes).filter(c -> c[1].equals(type)).count()) // type에 해당하는 것들만 filter해서 얻어오면
+                    .map(c -> c + 1) // 필터로 얻어 온것들을 1을 더해서
+                    .reduce(1, (c, n) -> c * n); // 누적해서 곱한 값 구하고
+
+            return answer - 1; // 최종값에 -1 을 해서 리턴한다.
+        }
+
+        // solution2를 리팩토링 한 것.
+        public int solution3(String[][] clothes) {
+            return Arrays.stream(clothes)
                     .map(c -> c[1])
                     .distinct()
-                    .map(type -> (int)Arrays.stream(clothes).filter(c -> c[1].equals(type)).count())
+                    .map(type -> (int) Arrays.stream(clothes).filter(c -> c[1].equals(type)).count())
                     .map(c -> c + 1)
-                    .reduce(1, (c, n) -> c * n);
-
-            return answer - 1;
+                    .reduce(1, (c, n) -> c * n) -1;
         }
     }
 
